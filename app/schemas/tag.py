aff48543402603a78ad2fc298298b5e518d79e7b
israDev <villar_80@hotmail.com>
2026-04-28 23:54:21 +0200

    feat: Fix Recent Tag Bug, change counter to Optional

--- app/schemas/tag.py
+++ app/schemas/tag.py
@@ -9,9 +9,9 @@
 
 #  dependencias
 from pydantic import BaseModel
-
+from typing import Optional
 
 # respuesta para las Tag generadas
 class TagResponse(BaseModel):
     name: str #tag
-    counter: int # cuantas veces se ha usado
+    counter: Optional[int]=0 # cuantas veces se ha usado
