eed5576a0f9ac352850db1bdd8bcebc1eb1ff105
israDev <villar_80@hotmail.com>
2026-05-19 22:57:31 +0200

    feat: add created_At to UserLeaderBoard response

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -30,6 +30,8 @@ class UserLeaderboard(BaseModel):
     avatar_url: Optional[str]= None
     reputation: int
     id: int
+    created_at: datetime
+
     model_config= ConfigDict(from_attributes = True)
 
 class UserStats(BaseModel):
